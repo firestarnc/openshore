4daf7f7553d2872462b2ae915ba45b4de76e2116
firestarnc <115705609+firestarnc@users.noreply.github.com>
2026-03-01 17:19:02 +0100

    updating package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Services from "@/components/services";
 import FeaturedWork from "@/components/featured-work";
 import Footer from '@/components/footer';
 
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
